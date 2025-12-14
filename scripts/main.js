a65347c70873de5bc7c767b2c485bb09fe98f8a4
lapointek <lapointek.dev@gmail.com>
2025-12-14 17:57:23 -0500

    feat: add script for button to go back-to-top on scroll style: add CSS style for button

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+let myButton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+    ) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
